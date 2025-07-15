584ff23de6ae83ca18d3807ec55fbb44c536c7d0
Kübra Dönmez <kubradonmezz@outlook.com.tr>
2025-07-15 22:05:31 +0300

    [JA-4] - JDBC ile tablo oluştur

--- Bebka-database/src/main/java/com/kubra/Main.java
+++ Bebka-database/src/main/java/com/kubra/Main.java
@@ -1,10 +1,26 @@
+import com.kubra.config.DataBaseConfig;
 import com.kubra.config.DataBaseConnectorConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Bağlantıyı başlat
-        DataBaseConnectorConfig.setConnection();
+        String sql="CREATE TABLE IF NOT EXISTS users ("+
+                "id SERIAL PRIMARY KEY,"+
+                "name VARCHAR(100),"+
+                "email VARCHAR(100))";
+        try{
+            Connection connection= DriverManager.getConnection(DataBaseConfig.DATABASE_URL,
+                    DataBaseConfig.DATABASE_USERNAME, DataBaseConfig.DATABASE_PASSWORD);
+            Statement statement=connection.createStatement();
+            statement.execute(sql);
+            System.out.println("Table Created");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
